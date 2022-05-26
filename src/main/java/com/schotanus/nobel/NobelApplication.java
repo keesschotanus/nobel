1f224ea3ea6d423501b310b607f33651265398b3
keesschotanus <kees.schotanus@planet.nl>
2022-05-26 11:43:21 +0200

    master Initial commit
    Commit after proper configuration and being able to run the program.
    The current configuration uses SqlServer.
    No password has been added to application.properties.
    Instead I configured my launch.json file to contain the password.

---
+++ src/main/java/com/schotanus/nobel/NobelApplication.java
@@ -0,0 +1,13 @@
+package com.schotanus.nobel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schotanus/nobel/NobelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schotanus.nobel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
